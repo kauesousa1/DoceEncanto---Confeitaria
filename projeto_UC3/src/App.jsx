cd6e449cd6b103c43ef553abc4d96bbd46633034
kauesousa1 <kauers2007@gmail.com>
2026-05-28 00:34:39 -0300

    pronto

--- projeto_UC3/src/App.jsx
+++ projeto_UC3/src/App.jsx
@@ -5,7 +5,8 @@ import About        from "./components/About";
 import Menu         from "./components/Menu";
 import Testimonials from "./components/Testimonials";
 import Contact      from "./components/Contact";
-import { SmoothScrollHero } from "./SmoothScrollHero";
+import Footer       from "./components/Footer";
+import { SmoothScrollHero } from "./SmoothScrollhero";
 
 export default function App() {
   return (
