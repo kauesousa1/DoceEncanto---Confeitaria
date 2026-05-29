c9007f2ba5b0cf0226819d47d7b872ab3d04a1e8
kauesousa1 <kauers2007@gmail.com>
2026-05-29 12:58:10 -0300

    atualização navbar

--- projeto_UC3/src/components/Navbar.jsx
+++ projeto_UC3/src/components/Navbar.jsx
@@ -2,6 +2,9 @@ import "../Styles/Navbar.css";
 
 const LINKS = ["Início", "Cardápio", "Depoimentos", "Contato"];
 
+const toId = (label) =>
+  label.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "").replace(/\s+/g, "-");
+
 export default function Navbar() {
   return (
     <div className="navbar__wrapper">
@@ -11,7 +14,7 @@ export default function Navbar() {
         <ul className="navbar__links navbar__links--left">
           {LINKS.slice(0, 2).map((l) => (
             <li key={l}>
-              <a href={`#${l.toLowerCase()}`} className="navbar__link">{l}</a>
+              <a href={`#${toId(l)}`} className="navbar__link">{l}</a>
             </li>
           ))}
         </ul>
@@ -23,7 +26,7 @@ export default function Navbar() {
         <ul className="navbar__links navbar__links--right">
           {LINKS.slice(2).map((l) => (
             <li key={l}>
-              <a href={`#${l.toLowerCase()}`} className="navbar__link">{l}</a>
+              <a href={`#${toId(l)}`} className="navbar__link">{l}</a>
             </li>
           ))}
         </ul>
